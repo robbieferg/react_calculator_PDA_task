7d2936e35ed34002d03d1c4e818fbc0468f64eef
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-01-05 20:14:49 +0000

    Concatenate and chain tests passed.

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -54,6 +54,31 @@ describe('Calculator', () => {
     const total = container.find("#running-total");
     expect(total.text()).toEqual("3");
   });
+  
+  it("Should be able to concatenate numbers", () => {
+    container.find("#number1").simulate("click");
+    container.find("#number2").simulate("click");
+    container.find("#number3").simulate("click");
+    container.find("#operator-equals").simulate("click");
+    const total = container.find("#running-total");
+    expect(total.text()).toEqual("123");
+  });
+  
+  it("Should be able to chain operations", () => {
+    container.find("#number2").simulate("click");
+    container.find("#operator-add").simulate("click");
+    container.find("#number3").simulate("click");
+    container.find("#operator-multiply").simulate("click");
+    container.find("#number4").simulate("click");
+    container.find("#operator-divide").simulate("click");
+    container.find("#number2").simulate("click");
+    container.find("#operator-subtract").simulate("click");
+    container.find("#number1").simulate("click");
+    container.find("#operator-equals").simulate("click");
+
+    const total = container.find("#running-total");
+    expect(total.text()).toEqual("9");
+  });
 
 
 
