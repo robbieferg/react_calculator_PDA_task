614064108c02596258625c0ec198aa1a22fe0d79
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-01-05 21:45:17 +0000

    All tests completed and passing.

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -54,12 +54,47 @@ describe("Calculator", () => {
     cy.get(".display").should("contain", "4");
   });
 
+  it("Should display positive numbers", () => {
+    cy.get("#number1").click();
+    cy.get(".display").should("not.contain", "-");
+  })
+
+  it("Should display negative numbers", () => {
+    cy.get("#number1").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number2").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "-1");
+  });
+  
+  it("Should display decimal numbers", () => {
+    cy.get("#number5").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number2").click();
+    cy.get("#decimal").click();
+    cy.get("#number5").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "2.5");
+  });
+
+  it("Should display very large numbers", () => {
+    for (let n = 0; n < 9; n++) {
+      cy.get("#number9").click();
+    };
+    cy.get("#operator-multiply").click();
+    for (let n = 0; n < 9; n++) {
+      cy.get("#number9").click();
+    };
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "999999998000000000");
+  });
+
   it("Should be able to divide by 0", () => {
     cy.get("#number1").click();
     cy.get("#operator-divide").click();
     cy.get("#number0").click();
     cy.get("#operator-equals").click();
     cy.get(".display").should("contain", "ERROR");
-  })
+  });
 
 });
