e514a837883da9f072b3ccd657533e01ffce7eb7
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-01-05 20:47:45 +0000

    Integrated operator tests passed.

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -1,10 +1,40 @@
 describe("Calculator", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
-  })
+  });
 
-  it('should have working number buttons', () => {
+  it('Should have working number buttons', () => {
     cy.get('#number2').click();
-    cy.get('.display').should('contain', '2')
-  })
-})
+    cy.get('.display').should('contain', '2');
+  });
+
+  it("Should have working add operator", () => {
+    cy.get("#number1").click();
+    cy.get("#operator-add").click();
+    cy.get("#number2").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "3");
+  });
+  it("Should have working subtract operator", () => {
+    cy.get("#number5").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number3").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "2");
+  });
+  it("Should have working multiply operator", () => {
+    cy.get("#number2").click();
+    cy.get("#operator-multiply").click();
+    cy.get("#number4").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "8");
+  });
+  it("Should have working divide operator", () => {
+    cy.get("#number9").click();
+    cy.get("#operator-divide").click();
+    cy.get("#number3").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "3");
+  });
+
+});
