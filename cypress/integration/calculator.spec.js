fcf8dd9869775e3f7c809667b0b96a9f4eaa8b94
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-01-05 20:54:12 +0000

    Cypress chain operators test passed.

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -15,6 +15,7 @@ describe("Calculator", () => {
     cy.get("#operator-equals").click();
     cy.get(".display").should("contain", "3");
   });
+  
   it("Should have working subtract operator", () => {
     cy.get("#number5").click();
     cy.get("#operator-subtract").click();
@@ -22,6 +23,7 @@ describe("Calculator", () => {
     cy.get("#operator-equals").click();
     cy.get(".display").should("contain", "2");
   });
+  
   it("Should have working multiply operator", () => {
     cy.get("#number2").click();
     cy.get("#operator-multiply").click();
@@ -29,6 +31,7 @@ describe("Calculator", () => {
     cy.get("#operator-equals").click();
     cy.get(".display").should("contain", "8");
   });
+  
   it("Should have working divide operator", () => {
     cy.get("#number9").click();
     cy.get("#operator-divide").click();
@@ -36,5 +39,19 @@ describe("Calculator", () => {
     cy.get("#operator-equals").click();
     cy.get(".display").should("contain", "3");
   });
+
+  it("Should be able to chain operations", () => {
+    cy.get("#number1").click();
+    cy.get("#operator-add").click();
+    cy.get("#number2").click();
+    cy.get("#operator-multiply").click();
+    cy.get("#number3").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number1").click();
+    cy.get("#operator-divide").click();
+    cy.get("#number2").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "4");
+  })
 
 });
